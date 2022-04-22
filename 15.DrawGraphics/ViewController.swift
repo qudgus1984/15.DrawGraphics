58dbe99dbb3d7fbc6a5fc84eca5d125b177ba5d6
ByeongHyeon <qudgus1984@naver.com>
2022-04-22 16:52:06 +0900

    원 그리기 완료

--- 15.DrawGraphics/ViewController.swift
+++ 15.DrawGraphics/ViewController.swift
@@ -41,9 +41,42 @@ class ViewController: UIViewController {
         imgView.image = UIGraphicsGetImageFromCurrentImageContext() // 현재 콘텍스트에 그려진 이미지를 가지고 와서 이미지 뷰에 나타남
         UIGraphicsEndImageContext() // 그림 그리기를 끝냄
     }
+    
     @IBAction func btnDrawRectangle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // Draw Rectangle
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addRect(CGRect(x: 70, y: 100, width: 200, height: 200)) // XY 좌표에서 시작하고 폭이 200픽셀, 높이가 200픽셀인 사각형을 그림
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
+    
     @IBAction func btnDrawCircle(_ sender: UIButton) {
+        UIGraphicsBeginImageContext(imgView.frame.size)
+        let context = UIGraphicsGetCurrentContext()!
+        
+        // Draw Ellipse
+        context.setLineWidth(2.0)
+        context.setStrokeColor(UIColor.red.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 50, width: 200, height: 100)) // XY 좌표에서 시작하고 폭이 200픽셀, 높이가 100픽셀인 타원을 그림
+        context.strokePath()
+        
+        // Draw Circle
+        context.setLineWidth(5.0)
+        context.setStrokeColor(UIColor.green.cgColor)
+        
+        context.addEllipse(in: CGRect(x: 70, y: 200, width: 200, height: 200)) // XY 좌표에서 시작하고 폭이 200픽셀, 높이가 200픽셀인 원을 그림
+        context.strokePath()
+        
+        imgView.image = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
     }
     @IBAction func btnDrawArc(_ sender: UIButton) {
     }
